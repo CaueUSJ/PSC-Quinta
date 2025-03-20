d9054547235fb32d59d0ad266a40a6f09594f644
Cauê Lopes Justo - 82329174 <82329174@ulife.com.br>
2025-03-20 11:34:16 -0300

    Matrizs e vetores v1.0

---
+++ Exemplo.java
@@ -0,0 +1,11 @@
+public class Exemplo {
+    public static void main(String[] args) {
+        int numero[] = new int[4];
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
